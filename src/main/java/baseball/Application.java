4b7b6696390f73992c9c73bab691507f9db73ae9
yangsso <didthglaso@gmail.com>
2022-04-14 16:37:28 +0900

    결과 세팅 + 결과값 노출 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,34 +7,47 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-//        입력받는다. 3번의 기회가 주어진다. 맞추면 종료 계속하려면 1 아니면 2
-        List<Integer> pickedNumbers = Randoms.pickUniqueNumbersInRange(1, 9, 3);
-
         int i = 0;
+        List<Integer> answer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
         while (true){
             String input = Console.readLine();
-            int[] result = check(input);
+            int[] result = check(input, answer);
             System.out.println(getResultMsg(result));
             if (result[0] == 3){
                 i = 3;
             }
             i++;
             if (i > 3){
-                Boolean wantContinue = Console.readLine().equals("1");
+                boolean wantContinue = Console.readLine().equals("1");
                 if (!wantContinue) break;
                 i = 0;
+                answer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
             }
         }
     }
 
-    public static int[] check(String input){
+    public static int[] check(String input, List<Integer> answer){
         return new int[]{};
     }
 
     public static String getResultMsg(int[] result){
+        String resultMsg = "";
+
         int strike = result[0];
         int ball = result[1];
-        int nothing = result[2];
-        return "";
+
+        if (ball > 0) {
+            resultMsg += String.format("%s볼", ball);
+        }
+
+        if (strike > 0) {
+            resultMsg += String.format("%s스트라이크", strike);
+        }
+
+        if (ball == 0 && strike == 0){
+            resultMsg = "낫싱";
+        }
+
+       return resultMsg;
     }
 }
